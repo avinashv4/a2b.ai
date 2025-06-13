08b97d42adaadfcf498b1114d40288a431883733
Avinash Venugopal <114874380+avinashv4@users.noreply.github.com>
2025-06-13 21:00:21 +0530

    Updated FlowingMenu.tsx

--- components/FlowingMenu.tsx
+++ components/FlowingMenu.tsx
@@ -81,7 +81,7 @@ const MenuItem: React.FC<MenuItemProps> = ({ link, text, image }) => {
   const repeatedMarqueeContent = React.useMemo(() => {
     return Array.from({ length: 4 }).map((_, idx) => (
       <React.Fragment key={idx}>
-        <span className="text-[#060010] uppercase font-normal text-[4vh] leading-[1.2] p-[1vh_1vw_0]">
+        <span className="text-[#ffffff] uppercase font-normal text-[4vh] leading-[1.2] p-[1vh_1vw_0]">
           {text}
         </span>
         <div
